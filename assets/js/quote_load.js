cbe9bcd48abad35ddca87c843c77c5e5b45ac268
Dina <Dina@Dina-2.local>
2014-05-08 13:24:47 +0400

    heroku db config

--- assets/js/quote_load.js
+++ assets/js/quote_load.js
@@ -1,7 +1,6 @@
 $(function() {
 
   socket.get('/get_quote', {}, function(response){
-    console.log(response);
     $('#randomimage').append('<p class="slog">' + response.quote.text +'<a href="/age/' + response.quote.age_id + '"> ' + response.quote.year +'</a></p>')
   });
 
