887731ef741af4ad2eaae684e5c4135edc8a7fe2
Dina <Dina@dina-2.dlink>
2014-05-19 22:04:01 +0400

    Lazy load

---
+++ assets/js/lazy_mine.js
@@ -0,0 +1,16 @@
+$(function() {
+  $("img.lazy").lazyload({
+    effect: 'fadeIn'
+  });
+  $("a.lazy").lazyload({
+    effect: 'fadeIn'
+  });
+});
+// $(function() {
+//   $("div.pop").lazyload({
+//     effect: 'fadeIn'
+//   });
+//   $("div.pop2").lazyload({
+//     effect: 'fadeIn'
+//   });
+// });
