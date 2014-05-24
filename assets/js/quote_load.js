511e6d6822a12e78a458798a712f33f871400c9c
Dina <Dina@dina-2.dlink>
2014-05-24 09:58:50 +0400

    quote

--- assets/js/quote_load.js
+++ assets/js/quote_load.js
@@ -1,7 +1,7 @@
 $(function() {
 
   socket.get('/get_quote', {}, function(response){
-    $('#randomimage').append('<p class="slog">' + response.quote.text +'<a href="http://cinema-project.herokuapp.com/age/' + response.quote.age_id + '"> ' + response.quote.year +'</a></p>')
+    $('#randomimage').append('<p class="slog">' + response.quote.text +'&nbsp;<a href="http://cinema-project.herokuapp.com/age/'+ response.quote.age_id + '">' + response.quote.year +'</a></p>')
   });
 
 });
